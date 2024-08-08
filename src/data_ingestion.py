a08e3ce575d9667ed24848957fcd08e3a2292c76
rajatchauhan99 <rajatchauhan99@gmail.com>
2024-08-08 09:03:12 +0530

    initial commit

---
+++ src/data_ingestion.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+import os
+
+
+df = pd.read_csv('https://raw.githubusercontent.com/campusx-official/jupyter-masterclass/main/tweet_emotions.csv')
+
+
+
+raw_data_path = os.path.join("./data", 'raw')
+
+os.makedirs(raw_data_path, exist_ok=True)
+
+df.to_csv(os.path.join(raw_data_path, "raw.csv"), index = False)
+
+
+df = df[3:]
+
+processed_data_path = os.path.join("./data", 'processed')
+
+os.makedirs(processed_data_path, exist_ok=True)
+
+df.to_csv(os.path.join(processed_data_path, "processed.csv"), index = False)
+
+
